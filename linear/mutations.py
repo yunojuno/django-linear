6e03c79f80118cfd02f997656fe4da415328aa84
Hugo Rodger-Brown <hugo@yunojuno.com>
2021-06-10 15:40:07 +0100

    Update create_issue GQL

--- linear/mutations.py
+++ linear/mutations.py
@@ -4,27 +4,27 @@ from linear.queries import run_query
 def create_issue(team_id: str, title: str, description: str, label_id: str) -> None:
     """Create a new issue in Linear."""
     mutation = """
-      mutation (
+        mutation (
           $title: String!
           $description: String!
           $teamId: String!
-          $labelId: String
-      ) {
-      issueCreate(
-          input: {
-          title: $title
-          description: $description
-          teamId: $teamId
-          labelIds: [$labelId]
-          }
-      ) {
-          success
-          issue {
-          id
-          title
+          $labelId: String!
+        ) {
+          issueCreate(
+            input: {
+              title: $title
+              description: $description
+              teamId: $teamId
+              labelIds: [$labelId]
+            }
+          ) {
+            success
+            issue {
+              id
+              title
+            }
           }
         }
-      }
     """
     run_query(
         mutation,
