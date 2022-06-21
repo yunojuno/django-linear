92f359b064ee7f47de5651991194bce537473aa5
Hugo Rodger-Brown <hugo@yunojuno.com>
2022-06-21 13:03:26 +0100

    Add subscriber_id to create_issue query

--- linear/mutations.py
+++ linear/mutations.py
@@ -1,29 +1,39 @@
 from linear.queries import run_query
 
 
-def create_issue(team_id: str, title: str, description: str, label_id: str) -> None:
+def create_issue(
+    team_id: str,
+    title: str,
+    description: str,
+    label_id: str,
+    subscriber_id: str,
+) -> None:
     """Create a new issue in Linear."""
+    # need to pass empty JS array, not [""]
+    subscriber_ids = [subscriber_id] if subscriber_id else []
     mutation = """
         mutation (
-          $title: String!
-          $description: String!
-          $teamId: String!
-          $labelId: String!
+        $title: String!
+        $description: String!
+        $teamId: String!
+        $labelId: String!
+        $subscriberIds: [String!]
         ) {
-          issueCreate(
+        issueCreate(
             input: {
-              title: $title
-              description: $description
-              teamId: $teamId
-              labelIds: [$labelId]
+            title: $title
+            description: $description
+            teamId: $teamId
+            labelIds: [$labelId]
+            subscriberIds: $subscriberIds
             }
-          ) {
+        ) {
             success
             issue {
-              id
-              title
+            id
+            title
             }
-          }
+        }
         }
     """
     run_query(
@@ -32,4 +42,5 @@ def create_issue(team_id: str, title: str, description: str, label_id: str) -> N
         title=title,
         description=description,
         labelId=label_id,
+        subscriberIds=subscriber_ids,
     )
